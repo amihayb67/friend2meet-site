87089858db848667eec1b48b695df6649e9460ff
amihayb67 <112643433+amihayb67@users.noreply.github.com>
2025-06-01 19:09:28 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,7 @@ function App() {
 {/*         <h2 className="text-xl md:text-2xl mb-8 tracking-wide">FUTURE OF DATING</h2> */}
 
         <div className="w-full max-w-md mx-auto rounded-full h-4 mb-8" style={{ backgroundColor: '#FCF5F0' }}>
-          <div className="h-4 rounded-full w-1/2 transition-all duration-500" style={{ backgroundColor: '#AFBDD6' }}></div>
+          <div className="h-4 rounded-full w-2/3 transition-all duration-500" style={{ backgroundColor: '#A7BEDF' }}></div>
         </div>
 {/*         <div className="w-full max-w-md mx-auto bg-gray-800 rounded-full h-4 mb-8">
           <div className="bg-blue-500 h-4 rounded-full w-1/2 transition-all duration-500"></div>
