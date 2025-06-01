6b1a71d1bcb546f7c692754784d20034cc6d5e68
amihayb67 <112643433+amihayb67@users.noreply.github.com>
2025-06-01 18:44:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -25,9 +25,9 @@ function App() {
         </h1>
 {/*         <h1 className="text-4xl md:text-6xl font-bold mb-6">UNDER CONSTRUCTION</h1> */}
 
-         <h2 className="text-2xl md:text-3xl tracking-[0.5em] text-center font-barlow mb-8">
+        <h2 className="text-xl md:text-2xl tracking-[0.5em] text-center font-barlow mb-8">
           FUTURE OF DATING
-        </h2>  
+        </h2>
 {/*         <h2 className="text-xl md:text-2xl mb-8 tracking-wide">FUTURE OF DATING</h2> */}
 
         <div className="w-full max-w-md mx-auto bg-gray-800 rounded-full h-4 mb-8">
